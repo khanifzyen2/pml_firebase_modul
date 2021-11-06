80d2d82e1832daa9c63acc60c93e32cc17123d72
Akhmad Khanif Zyen <khanif.zyen@unisnu.ac.id>
2021-11-06 14:53:41 +0700

    modul 4 done

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,35 @@
 import 'package:flutter/material.dart';
-
+import 'package:firebase_core/firebase_core.dart';
 import 'package:get/get.dart';
-
+import 'app/utils/error_page.dart';
 import 'app/routes/app_pages.dart';
+import 'app/utils/loading_page.dart';
 
 void main() {
-  runApp(
-    GetMaterialApp(
-      title: "Application",
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
-    ),
-  );
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshots) {
+        if (snapshots.hasError) {
+          return ErrorPage();
+        }
+        if (snapshots.connectionState == ConnectionState.done) {
+          return GetMaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: "Chat App",
+            initialRoute: AppPages.INITIAL,
+            getPages: AppPages.routes,
+          );
+        }
+        return LoadingPage();
+      },
+    );
+  }
 }
