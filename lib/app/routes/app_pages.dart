6dbefdaa3899d4c631991a1941588ba8f7991d62
Akhmad Khanif Zyen <khanif.zyen@unisnu.ac.id>
2021-11-06 15:23:17 +0700

    create introduction and login page

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -2,6 +2,10 @@ import 'package:get/get.dart';
 
 import 'package:pml_firebase_modul/app/modules/home/bindings/home_binding.dart';
 import 'package:pml_firebase_modul/app/modules/home/views/home_view.dart';
+import 'package:pml_firebase_modul/app/modules/introduction/bindings/introduction_binding.dart';
+import 'package:pml_firebase_modul/app/modules/introduction/views/introduction_view.dart';
+import 'package:pml_firebase_modul/app/modules/login/bindings/login_binding.dart';
+import 'package:pml_firebase_modul/app/modules/login/views/login_view.dart';
 
 part 'app_routes.dart';
 
@@ -15,6 +19,16 @@ class AppPages {
       name: _Paths.HOME,
       page: () => HomeView(),
       binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.INTRODUCTION,
+      page: () => IntroductionView(),
+      binding: IntroductionBinding(),
+    ),
+    GetPage(
+      name: _Paths.LOGIN,
+      page: () => LoginView(),
+      binding: LoginBinding(),
     ),
   ];
 }
